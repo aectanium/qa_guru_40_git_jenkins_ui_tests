b1c9f9d7df85aa4d6e84e61c3b06f2c8a129af86
Dyakonov <aectan1987@ya.ru>
2026-05-17 10:16:59 +0300

    added tests with steps and pageobject

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,7 +6,10 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationPage;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -26,7 +29,13 @@ public class TestBase {
 //        Configuration.browser = "chrome";
 //        Configuration.browserVersion = "128.0";
 //        Configuration.browserVersion = "130.0";
-        Configuration.remote = "https://user1:1234@ru.selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
     @AfterEach
     void addAttachments() {
