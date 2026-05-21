ed1a6c2978526136eaaa7b67a821e3e1c0910b76
Dyakonov <aectan1987@ya.ru>
2026-05-21 16:27:29 +0300

    change attach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,5 +1,4 @@
 package tests;
-
 import helpers.Attach;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
@@ -24,28 +23,17 @@ public class TestBase {
     }
 
     @BeforeAll
-    static void beforeAll() {
-
-        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
-
-
-        String browser = System.getProperty("browser", "chrome");
-        String browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.browser = browser;
-        Configuration.browserVersion = browserVersion;
-
-
+    static void setupSelenideConfig() {
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-
-
+        Configuration.baseUrl = System.getProperty("baseUrl");
+        Configuration.pageLoadStrategy = "eager";
         Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
 
-
-        String remote = System.getProperty("remote", "");
-        if (remote != null && !remote.isEmpty()) {
-            Configuration.remote = remote;
-        }
-
+        String loginSelenoid = System.getProperty("loginSelenoid");
+        String passwordSelenoid = System.getProperty("passwordSelenoid");
+        String urlSelenoid = System.getProperty("urlSelenoid");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
@@ -53,8 +41,8 @@ public class TestBase {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://" + loginSelenoid + ":" + passwordSelenoid + "@" + urlSelenoid;
     }
-
     @AfterEach
     void addAttachments() {
         Attach.screenshotAs();
