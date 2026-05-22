039f89ead22db30be44a8efe252b2c332423089e
Dyakonov <aectan1987@ya.ru>
2026-05-22 10:45:17 +0300

    change TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -24,26 +24,20 @@ public class TestBase {
 
     @BeforeAll
     static void setupSelenideConfig() {
-
         Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "126.0");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         Configuration.pageLoadStrategy = "eager";
         Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "true"));
 
-        String environment = System.getProperty("environment", "demo");
         String remoteUrl = System.getProperty("remoteUrl");
-
-        System.out.println("Environment: " + environment);
-        System.out.println("Browser: " + Configuration.browser + " " + Configuration.browserVersion);
-        System.out.println("Headless: " + Configuration.headless);
+        String environment = System.getProperty("environment", "demo");
 
         if (remoteUrl != null && !remoteUrl.isEmpty()) {
+            Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
             System.out.println("Using remote WebDriver: " + remoteUrl);
 
             DesiredCapabilities capabilities = new DesiredCapabilities();
-
             boolean enableVideo = Boolean.parseBoolean(System.getProperty("enableVideo", "false"));
             Map<String, Object> selenoidOptions = Map.<String, Object>of(
                     "enableVNC", true,
@@ -55,9 +49,17 @@ public class TestBase {
             capabilities.setCapability("selenoid:options", selenoidOptions);
             Configuration.browserCapabilities = capabilities;
             Configuration.remote = remoteUrl;
+
         } else {
-            System.out.println("Using local WebDriver");
+            System.out.println("Using local WebDriver - letting Selenium Manager determine version");
+            Configuration.browserVersion = "";
         }
+
+        System.out.println("=== Configuration ===");
+        System.out.println("Browser: " + Configuration.browser);
+        System.out.println("Version: " + Configuration.browserVersion);
+        System.out.println("Remote: " + Configuration.remote);
+        System.out.println("========================");
     }
 
     @AfterEach
